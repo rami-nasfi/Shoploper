5df6b9884408f4f778baacbceb79e67b701494e3
Mohamed Rami Nasfi <med.rami.nasfi@gmail.com>
2024-05-30 18:21:40 +0100

    test dploy

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 const main = require("./Connection");
